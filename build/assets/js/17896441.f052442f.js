66727dd6f2037f06861879c3e926a7f3ec3e0c42
Ayesha-p <64060103+Ayesha-p@users.noreply.github.com>
2025-12-18 11:26:57 +0500

    Update build for gh-pages deployment

---
+++ build/assets/js/17896441.f052442f.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkphysical_ai_humanoid_robotics_book=globalThis.webpackChunkphysical_ai_humanoid_robotics_book||[]).push([[8401],{18506:(o,i,a)=>{a.d(i,{A:()=>s});a(96540);const s={...a(17010).A}}}]);
---
+++ build/assets/js/a7456010.6f34957c.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkphysical_ai_humanoid_robotics_book=globalThis.webpackChunkphysical_ai_humanoid_robotics_book||[]).push([[1235],{88552:a=>{a.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
